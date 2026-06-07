58109d9176a894375c2552d162c961d8db77302b
suyash7532-crypto <suyashgupta4532@gmail.com>
2026-06-07 12:23:06 +0530

    first project as begginer

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int add()
+{
+    int a, b;
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    return a + b;
+}
+
+int main()
+{
+    std::cout << "is this world even real?" << std::endl;
+    std::cout << "or just we are manipulated?" << std::endl;
+    return 0;
+}
